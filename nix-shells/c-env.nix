ce2bed21cea41a4e3d1d1fc3374c6cc2b308df76
Valery Ivanov <ivalery111@gmail.com>
2021-01-04 19:59:27 +0300

    shell: c-env: adds initial  necessary packages
    * gcc 10
    * clang 11
    * clang tools
    * clang analyzer
    * cmake
    * vim
    * gdb
    * valgrind

--- nix-shells/c-env.nix
+++ nix-shells/c-env.nix
@@ -1,7 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
   pkgs.mkShell {
-      nativeBuildInputs = [ 
+      nativeBuildInputs = [
+        pkgs.buildPackages.vim
+
+        pkgs.buildPackages.gcc10
+        pkgs.buildPackages.clang_11
+        pkgs.buildPackages.clang-tools
+        pkgs.buildPackages.clang-analyzer
+
         pkgs.buildPackages.cmake 
       ];
   }
