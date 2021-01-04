56faea4af6561c5669bf8cf86bf3df6596d3ac68
Valery Ivanov <ivalery111@gmail.com>
2021-01-04 20:10:27 +0300

    shell: c-env: adds valgrind and gdb

--- nix-shells/c-env.nix
+++ nix-shells/c-env.nix
@@ -6,8 +6,11 @@
 
         pkgs.buildPackages.gcc10
         pkgs.buildPackages.clang_11
+
         pkgs.buildPackages.clang-tools
         pkgs.buildPackages.clang-analyzer
+        pkgs.buildPackages.valgrind
+        pkgs.buildPackages.gdb
 
         pkgs.buildPackages.cmake 
       ];
