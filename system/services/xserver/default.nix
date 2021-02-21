bb4ac3ad88f88a341c1570f70dfae2668ab86908
Valery Ivanov <ivalery111@gmail.com>
2021-02-21 16:13:00 +0300

    services: xserver: use i3 instead of plasma5

--- system/services/xserver/default.nix
+++ system/services/xserver/default.nix
@@ -1,19 +1,26 @@
 # TODO(valery): Split to the subodules like a display manager, desktop manager, ...
+{ config, pkgs, callPackage, ... }:
 
 {
   services.xserver = {
     enable = true;
 
-    displayManager.sddm = {
-      enable = true;
+    displayManager= {
+      defaultSession = "none+i3";
     };
 
-    desktopManager.plasma5 = {
-      enable = true;
+    desktopManager= {
+      xterm.enable = false;
     };
 
     windowManager.i3 = {
       enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+      ];
     };
 
     videoDrivers = [ "intel" ];
