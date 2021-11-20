efd6f790440f08033469f7d94aed013a036c63a7
Valery Ivanov <ivalery111@gmail.com>
2021-11-20 05:13:51 +0300

    root: add script for auto partition

---
+++ setup_partitions.sh
@@ -0,0 +1,21 @@
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart primary 512MiB -8GiB
+parted /dev/sda -- mkpart primary linux-swap -8GiB 100%
+parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/sda -- set 3 esp on
+
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+swapon /dev/sda2
+mkfs.fat -F 32 -n boot /dev/sda3        # (for UEFI systems only)
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot                      # (for UEFI systems only)
+mount /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
+nixos-generate-config --root /mnt
+
+echo "Setup configuration with:"
+echo "nano /mnt/etc/nixos/configuration.nix"
+echo "After that do:"
+echo "nixos-install"
+echo "reboot"
+
